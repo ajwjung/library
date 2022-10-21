c6f2d518fa551fd95ff2b9b6e853e85b6cf0edc1
Amy Jung <ajwjung@outlook.com>
2022-10-21 14:49:45 -0700

    Create function to add a book

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
@@ -6,6 +8,16 @@ function Book(title, author, pages, read) {
     this.info = function() {
         return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`
     }
+}
+
+function addBookToLibrary() {
+    const title = prompt("Enter a book title:");
+    const author = prompt("Who wrote the book?");
+    const pages = prompt("How many pages does the book have?");
+    const read = prompt("Did you finish reading the book?");
+
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
 }
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295 pages", "not read yet");
