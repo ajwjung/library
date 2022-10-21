6b28a7e9686bc6824b8c390c862b088b39eb04d2
Amy Jung <ajwjung@outlook.com>
2022-10-21 15:29:26 -0700

    feat: Create function to display books as "cards"

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+const container = document.querySelector(".container");
+
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -20,4 +22,40 @@ function addBookToLibrary() {
     myLibrary.push(newBook);
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295 pages", "not read yet");
+function displayBooks() {
+    myLibrary.forEach((book) => addCard(book))
+}
+
+function addCard(item) {
+    const newCard = document.createElement("div");
+    newCard.classList.add("card");
+
+    // Create new paragraph for each property
+    for (property in item) {
+        if (property == "info") continue;
+
+        const newPara = document.createElement("p");
+        const newContent = document.createTextNode(item[property]);
+        newPara.appendChild(newContent);     
+        newCard.appendChild(newPara);
+    }
+
+    newCard.style.border = "2px solid black";
+
+    container.appendChild(newCard);
+}
+
+// Temporary content for testing
+const theHobbit = new Book(
+    "The Hobbit", "J.R.R. Tolkien", "295 pages", "not read yet"
+    );
+const harryPotter = new Book(
+    "Harry Potter and the Sorcerer's Stone", "J. K. Rowling", "223 pages", "read"
+    );
+const percyJackson = new Book(
+    "The Lightning Thief", "J.R.R. Tolkien", "377 pages", "not read yet"
+    );
+
+myLibrary.push(theHobbit, harryPotter, percyJackson);
+
+displayBooks();
