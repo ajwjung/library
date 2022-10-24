d3c53115b00626249d203f5d20d0ae0f3d6a0e49
Amy Jung <ajwjung@outlook.com>
2022-10-24 14:27:57 -0700

    refactor: Remove unused code

--- scripts/script.js
+++ scripts/script.js
@@ -15,20 +15,7 @@ function Book(title, author, pages, read) {
     this.author = author
     this.pages = pages
     this.read = read
-    // this.info = function() {
-    //     return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`
-    // }
 }
-
-// function addBookToLibrary() {
-//     const title = prompt("Enter a book title:");
-//     const author = prompt("Who wrote the book?");
-//     const pages = prompt("How many pages does the book have?");
-//     const read = prompt("Did you finish reading the book?");
-
-//     let newBook = new Book(title, author, pages, read);
-//     myLibrary.push(newBook);
-// }
 
 function addBookToLibrary(title, author, pages, read) {
     let newBook = new Book(title, author, pages, read);
@@ -58,13 +45,13 @@ function displayBooks() {
     myLibrary.forEach((book) => addCard(book))
 }
 
+// Creates a single book card
 function addCard(item) {
     const newCard = document.createElement("div");
     newCard.classList.add("card");
 
     // Create new paragraph for each property
     for (property in item) {
-        // if (property == "info") continue;
 
         const newPara = document.createElement("p");
         const newContent = document.createTextNode(item[property]);
